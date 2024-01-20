df941a656b73c1ec88ed91716a16da7ef49aa6e0
onradschultz <157186809+onradschultz@users.noreply.github.com>
2024-01-19 22:01:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -38,3 +38,4 @@ if st.button('Moderate'):
     serialized_output = serialize(output)
     json_output = json.dumps(serialized_output, indent=2, ensure_ascii=False)
     st.json(json_output)
+st.text("hello")
